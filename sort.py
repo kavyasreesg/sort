c9f03215831c2411fcd7337fef356db17558073d
Kavyasree S G <kavyasreesg1997@gmail.com>
2020-12-18 12:56:17 +0530

    Add files via upload
    Sorting of a list

---
+++ sort.py
@@ -0,0 +1,15 @@
+# Built in
+list1 = [1, 2, 0, 3]
+list1.sort()
+print(list1)
+
+# Traditional way
+list2 = [1, 0, 3, 2, 5, 4]
+for i in range(len(list2) - 1):
+
+    for j in range(0, len(list2) - i - 1):
+
+        if list2[j] > list2[j + 1]:
+            list2[j], list2[j + 1] = list2[j + 1], list2[j]
+
+print(list2)
