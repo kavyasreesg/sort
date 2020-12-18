1f65d520a4d54dcf00dc1177b4c31f30e76a8297
Kavyasree S G <kavyasreesg1997@gmail.com>
2020-12-18 14:14:04 +0530

    Add files via upload

--- sort.py
+++ sort.py
@@ -3,6 +3,9 @@ list1 = [1, 2, 0, 3]
 list1.sort()
 print(list1)
 
+# using lambda
+x = lambda list3: sorted(list3)
+print(x([1, 0, 6, 5, 2]))
 # Traditional way
 list2 = [1, 0, 3, 2, 5, 4]
 for i in range(len(list2) - 1):
